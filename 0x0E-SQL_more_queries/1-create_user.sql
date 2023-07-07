05215f8debbd5c8e1c1834e792b95f8e556c7af4
Rah-7 <rahigirmam16@gmail.com>
2023-07-07 20:40:18 +0300

    host

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- This script lists all privileges of the MySQL users
--- user_0d_1 and user_0d_2 on your server (in localhost).
-
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- creates the MySQL server user user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
